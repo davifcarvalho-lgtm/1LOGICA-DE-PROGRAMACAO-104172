f29e7de02c6a2dfd866b79d151b82cd9d4e09c77
Davi Ferreira Orge Cafezeiro de Carvalho <davi.f.carvalho@ba.estudante.senai.br>
2026-08-21 16:47:55 -0300

    Add files via upload

---
+++ 6 - antecessor_e_sucessor.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("cls")
+
+print("= Solicitando dados =")
+numero = int(input("Digite um numero inteiro "))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print("\n= EXIBINDO DADOS = ")
+print("antecessor:", antecessor)
+print("sucessor:", sucessor)
