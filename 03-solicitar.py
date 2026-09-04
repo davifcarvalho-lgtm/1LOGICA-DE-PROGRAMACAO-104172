2953ee7881bbf3b93e02f063f3f606905378e4e1
Davi Ferreira Orge Cafezeiro de Carvalho <davi.f.carvalho@ba.estudante.senai.br>
2026-09-04 16:46:26 -0300

    Add files via upload

---
+++ 03-solicitar.py
@@ -0,0 +1,15 @@
+import os 
+os.system("cls")
+
+#inicio
+
+numero=float(input("digite o numero: "))
+max=20
+min=10
+
+#processo 
+
+if numero>=min and numero<=max:
+    print("Seu numero esta entre 10 e 20")
+
+
