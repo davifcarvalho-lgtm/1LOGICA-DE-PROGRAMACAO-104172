0969c43368471c380c4f013cea4684470190b901
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-23 14:56:55 -0300

    atividades de laço de repetição - for.

---
+++ 3-laços de repetição/2-exercicio.py
@@ -0,0 +1,9 @@
+import os
+os.system("cls")
+
+mumero=float(input("digite um numero para o calculo"))
+
+
+for i in range(1, 11):
+    print("divisao")
+    print(f"")
---
+++ 3-laços de repetição/6-atividade.py
@@ -0,0 +1,8 @@
+import os 
+os.system("cls")
+
+for i in range(10, 0 -1):
+    print(1)
+
+print("FIM")
+
